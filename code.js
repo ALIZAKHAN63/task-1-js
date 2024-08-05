1651558eee993a2af3cac9fe34b26f1572c828ab
ALIZAKHAN63 <alizakhan99163@gmail.com>
2024-08-05 20:38:44 +0500

    message for

---
+++ code.js
@@ -0,0 +1,11 @@
+var a = +prompt("enter your age")
+var  age = 16
+if (a == age){
+    alert("you are eligible")
+}
+else if (a == 17){
+    alert("you are not eligible")
+}
+else {
+    alert("sorry")
+}
